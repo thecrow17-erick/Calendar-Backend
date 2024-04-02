dd3a21c6662ea02e98aefe31e4e6dc836eb1995a
Erick Milton Aricari Blanco <82915996+thecrow17-erick@users.noreply.github.com>
2024-04-02 14:10:56 -0400

    Delete prisma/migrations directory

--- prisma/migrations/20240331042019_parallel_name/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `name` to the `Parallel` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Parallel" ADD COLUMN     "name" VARCHAR(50) NOT NULL;
